69570991b13ebc58f2bee22c549c67f1294d38f4
Bevence <sitikhubikesh@gmail.com>
2019-12-03 17:22:40 +0545

    Emergency Number added

--- app/src/main/java/com/example/bta/activities/Contact.java
+++ app/src/main/java/com/example/bta/activities/Contact.java
@@ -34,22 +34,23 @@ protected AdapterContact adapterContact;
 
     public void generateList(){
         arrayList = new ArrayList<>();
-        arrayList.add(new EmergencyContact("Bhaktapur Hospital","9843803828"));
-        arrayList.add(new EmergencyContact("Janswasthaya","5122035"));
-        arrayList.add(new EmergencyContact("Bhaktapur Hospital","9843803828"));
-        arrayList.add(new EmergencyContact("Janswasthaya","5122035"));
-        arrayList.add(new EmergencyContact("Bhaktapur Hospital","9843803828"));
-        arrayList.add(new EmergencyContact("Janswasthaya","5122035"));
-        arrayList.add(new EmergencyContact("Bhaktapur Hospital","9843803828"));
-        arrayList.add(new EmergencyContact("Janswasthaya","5122035"));
-        arrayList.add(new EmergencyContact("Bhaktapur Hospital","9843803828"));
-        arrayList.add(new EmergencyContact("Janswasthaya","5122035"));
-        arrayList.add(new EmergencyContact("Bhaktapur Hospital","9843803828"));
-        arrayList.add(new EmergencyContact("Janswasthaya","5122035"));
-        arrayList.add(new EmergencyContact("Bhaktapur Hospital","9843803828"));
-        arrayList.add(new EmergencyContact("Janswasthaya","5122035"));
-        arrayList.add(new EmergencyContact("Bhaktapur Hospital","9843803828"));
-        arrayList.add(new EmergencyContact("Janswasthaya","5122035"));
+        arrayList.add(new EmergencyContact("Fire Brigade","101"));
+        arrayList.add(new EmergencyContact("Police Control","100"));
+        arrayList.add(new EmergencyContact("Department of Immigration","01-4223509/ 01-4472257"));
+        arrayList.add(new EmergencyContact("Tribhuvan International Airport(TIA)","01-4472256/ 01-4472257"));
+        arrayList.add(new EmergencyContact("Electricity Bhaktapur(Power Failure)","01-6610065"));
+        arrayList.add(new EmergencyContact("Ambulance(Bhaktapur Municipality)","01-6613200/ 9851082799"));
+        arrayList.add(new EmergencyContact("Ambulance(Siddhi Smriti Hospital)","01-6610570/ 9841204991"));
+        arrayList.add(new EmergencyContact("Ambulance(Dr. Iwamura Hospital)","01-6612705"));
+        arrayList.add(new EmergencyContact("Ambulance(Bhaktapur Cancer Hospital)","01-6611532/ 9860171552"));
+        arrayList.add(new EmergencyContact("Ambulance(Red Cross)","01-6612266/ 9841489408"));
+        arrayList.add(new EmergencyContact("Ambulance(Madyapur Thimi Hospital)","9851133613"));
+        arrayList.add(new EmergencyContact("Bhaktapur Hospital","01-6610676"));
+        arrayList.add(new EmergencyContact("Blood Blank, Kathmandu","01-4229344/ 01-4225344"));
+        arrayList.add(new EmergencyContact("Blood Blank, Bhaktapur","01-6612266/ 01-6611661"));
+        arrayList.add(new EmergencyContact("Tilganga Eye Hospital","01-4250691"));
+        arrayList.add(new EmergencyContact("Bhaktapur RedCross","01-6612266"));
+        arrayList.add(new EmergencyContact("Madyapur Thimi Hospital","01-5093388"));
 
 
     }
