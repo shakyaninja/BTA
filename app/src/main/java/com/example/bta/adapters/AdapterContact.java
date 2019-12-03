99667dafef0dc65f4b258b5c234b10f191186191
shakyaninja <shakyaninja@gmail.com>
2019-12-03 19:31:42 +0545

    corrects strings

--- app/src/main/java/com/example/bta/adapters/AdapterContact.java
+++ app/src/main/java/com/example/bta/adapters/AdapterContact.java
@@ -1,18 +1,18 @@
 package com.example.bta.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.bta.R;
 import com.example.bta.modals.EmergencyContact;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
@@ -45,10 +45,22 @@ public class AdapterContact  extends RecyclerView.Adapter<AdapterContact.MyViewH
     public class MyViewHolder extends RecyclerView.ViewHolder{
         protected TextView serviceprovider;
         protected TextView contact;
-        public MyViewHolder(@NonNull View itemView) {
+        public MyViewHolder(@NonNull final View itemView) {
             super(itemView);
             serviceprovider = itemView.findViewById(R.id.emergencyServiceProvider);
             contact = itemView.findViewById(R.id.emergencyContact);
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    switch(getAdapterPosition()){
+                        case 0:
+                            Intent intent = new Intent(Intent.ACTION_CALL);
+                            Intent intent1 = intent.putExtra("phoneno", arrayList.get(getAdapterPosition()));
+                            break;
+                    }
+                    Toast.makeText(context, "contact:", Toast.LENGTH_SHORT).show();
+                }
+            });
         }
     }
 }
