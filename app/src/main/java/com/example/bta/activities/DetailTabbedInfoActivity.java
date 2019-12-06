4e501eeca73c9e694c9cd141b8ed93660527b274
Niranjan2054 <ckim6189@gmail.com>
2019-12-06 08:26:47 +0545

    Dattatraya, Pottery Square

--- app/src/main/java/com/example/bta/activities/DetailTabbedInfoActivity.java
+++ app/src/main/java/com/example/bta/activities/DetailTabbedInfoActivity.java
@@ -50,25 +50,35 @@ ViewPager viewPager;
                         "\tThis is the only temple that is named after the dimension of architecture rather than from the deity residing inside.",R.drawable.detailnyatapola), "Sky View");
                 adapter.addFragment(new DetailedFragment("The broad-fronted, triple-roofed Bhairabnath Temple is dedicated to Bhairab, the fearsome incarnation of Shiva, whose consort occupies the Nyatapola Temple across the square.",R.drawable.bhairav2), "God Bhairav");
                 break;
-//            case 2:
-//                titletext.setText(getText(R.string.dattatraya_title));
-//                adapter.addFragment(new FamousHereFragment("Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail."), "tab 1");
-//                adapter.addFragment(new MapFragment("hi you !!"), "tab 2");
-//                adapter.addFragment(new DetailedFragment("Hello everyone"), "tab 3");
-//                break;
-//            case 3:
-//                titletext.setText(getText(R.string.pottery_title));
-//                adapter.addFragment(new FamousHereFragment("Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail."), "tab 1");
-//                adapter.addFragment(new MapFragment("Khutruke are sukayo",R.drawable.potterysquare), "Khutruke");
-////                adapter.addFragment(new DetailedFragment("Hello everyone"), "tab 3");
-//                 break;
+            case 2:
+                titletext.setText(getText(R.string.dattatraya_title));
+                adapter.addFragment(new FamousHereFragment("At the east end of Tachupal Tole, the eye-catching Dattatreya Temple was originally built in 1427, supposedly using the timber only from two trees. \n" +
+                        "The temple is dedicated to Dattatreya, a curious hybrid deity, blending elements of Brahma, Vishnu and Shiva. Judging from the Garuda statue and the conch and " +
+                        "chakra disc mounted on pillars supported by stone turtles in front of the temple, Vishnu seems to have come out on top.\n",R.drawable.dattatarya), "Datttraya");
+                adapter.addFragment(new MapFragment("There is wood carving musieum in peacock windowed palace.\n" +
+                        "In front of peaceck windowed palace, there is another musium of old utensils of brass and bronze.",R.drawable.dattatraya2), "Meusem");
+                adapter.addFragment(new DetailedFragment("There is unique Peacock Window in Bhaktapur Nepal built in 15th century.\n" +
+                        "The Peacock Window is a 15th century window built in Bhaktapur Nepal. \n" +
+                        "Commonly referred to as the \"Mona Lisa\" of Nepal is it one of the countries most precious art works. \n" +
+                        "It consists several small bird carvings with a large intricately " +
+                        "decorated peacock carving in the center with tail feathers that form a lattice window.",R.drawable.akhijhyal), "Peacock Window");
+                adapter.addFragment(new DetailedFragment("Desay Madu Jhyā is a traditional wooden window in Bhaktapur which is celebrated for its uniqueness. The name means \"window without equal in the country\" in Nepal Bhasa.\n" +
+                        "Desay Madu Jhya is a latticed window with multiple frames. Its design looks like the bellows in an old folding camera.\n" +
+                        "Desay Madu Jhyā is a specimen of the woodcarving heritage of the Newar" +
+                        " people of Nepal which goes back more than a thousand years.",R.drawable.desamaru), "Desay Madu Jhya");
+
+                break;
+            case 3:
+                titletext.setText(getText(R.string.pottery_title));
+                adapter.addFragment(new FamousHereFragment("The world famous Juju dhau, curd made only in " +
+                        "Bhaktapur, is served in mud bowls,Kala are  made here. ",R.drawable.potterysquare), "Kala");
+                adapter.addFragment(new MapFragment("Lha Maru Lyasindyo:\n" +
+                        "Lha Maru Lasyindya is upstanded here in new year.\n" +
+                        "Is stoodup at Morning of new year eve. After it is stood up different rituals and pujas are done." +
+                        "It is kept standing for five days. It is stood up at pottery square.",R.drawable.biska3jatra), "Lha Maru lyasindyo");
+                 break;
             case 4:
             case 10:
-//                titletext.setText(getText(R.string.durbar_title));
-//                adapter.addFragment(new FamousHereFragment("Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail."), "tab 1");
-//                adapter.addFragment(new MapFragment("hi you !!"), "tab 2");
-//                adapter.addFragment(new DetailedFragment("Hello everyone"), "tab 3");
-//                break;
                 titletext.setText(getText(R.string.durbar_title));
                 adapter.addFragment(new FamousHereFragment("Golden Gate",R.drawable.durbarsq1), "Golder Gate");
                 adapter.addFragment(new MapFragment("Reconstructed Bastola Temple",R.drawable.durbarsq3), "Bustola Temple");
