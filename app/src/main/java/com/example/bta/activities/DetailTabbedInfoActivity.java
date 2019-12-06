d43e1137c7bfd963f92890a4f7736eff33d459d4
Niranjan2054 <ckim6189@gmail.com>
2019-12-06 08:12:25 +0545

    Content Updated: Nyatapola, Biska Jatra, Durbar Square

--- app/src/main/java/com/example/bta/activities/DetailTabbedInfoActivity.java
+++ app/src/main/java/com/example/bta/activities/DetailTabbedInfoActivity.java
@@ -41,12 +41,15 @@ ViewPager viewPager;
     public void setupViewPager(ViewPager viewPager,int key) {
         SectionsPageAdapter adapter = new SectionsPageAdapter(getSupportFragmentManager());
         switch(key){
-//            case 1:
-//                titletext.setText(getText(R.string.nyatapola_title));
-//                adapter.addFragment(new FamousHereFragment("Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail."), "tab 1");
-//                adapter.addFragment(new MapFragment("hi you !!"), "Map");
-//                adapter.addFragment(new DetailedFragment("Hello everyone"), "tab 3");
-//                break;
+            case 1:
+                titletext.setText(getText(R.string.nyatapola_title));
+                adapter.addFragment(new FamousHereFragment("The broad-fronted, triple-roofed Bhairabnath Temple is dedicated to Bhairab, the fearsome incarnation of Shiva, whose consort occupies the Nyatapola Temple across the square.",R.drawable.bhairav), "Bhairavnath");
+                adapter.addFragment(new MapFragment("It was built in only 17 months from the time it started.\n" +
+                        "\tIt was built in a time when the Taj Mahal was under construction.\n" +
+                        "\tIt is the tallest pagodas in the country and stands 30m high.\n" +
+                        "\tThis is the only temple that is named after the dimension of architecture rather than from the deity residing inside.",R.drawable.detailnyatapola), "Sky View");
+                adapter.addFragment(new DetailedFragment("The broad-fronted, triple-roofed Bhairabnath Temple is dedicated to Bhairab, the fearsome incarnation of Shiva, whose consort occupies the Nyatapola Temple across the square.",R.drawable.bhairav2), "God Bhairav");
+                break;
 //            case 2:
 //                titletext.setText(getText(R.string.dattatraya_title));
 //                adapter.addFragment(new FamousHereFragment("Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail."), "tab 1");
@@ -59,13 +62,18 @@ ViewPager viewPager;
 //                adapter.addFragment(new MapFragment("Khutruke are sukayo",R.drawable.potterysquare), "Khutruke");
 ////                adapter.addFragment(new DetailedFragment("Hello everyone"), "tab 3");
 //                 break;
-//            case 4:
-//            case 10:
+            case 4:
+            case 10:
 //                titletext.setText(getText(R.string.durbar_title));
 //                adapter.addFragment(new FamousHereFragment("Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail."), "tab 1");
 //                adapter.addFragment(new MapFragment("hi you !!"), "tab 2");
 //                adapter.addFragment(new DetailedFragment("Hello everyone"), "tab 3");
 //                break;
+                titletext.setText(getText(R.string.durbar_title));
+                adapter.addFragment(new FamousHereFragment("Golden Gate",R.drawable.durbarsq1), "Golder Gate");
+                adapter.addFragment(new MapFragment("Reconstructed Bastola Temple",R.drawable.durbarsq3), "Bustola Temple");
+                adapter.addFragment(new DetailedFragment("55 Window Palace",R.drawable.durbarsq2), "55 Window Palace");
+                break;
             case 11:
             case 22:
                 titletext.setText(getText(R.string.changu_title));
@@ -97,12 +105,12 @@ ViewPager viewPager;
 //                adapter.addFragment(new MapFragment("hi you !!"), "tab 2");
 //                adapter.addFragment(new DetailedFragment("Hello everyone"), "tab 3");
 //                break;
-//            case 16:
-//                titletext.setText(getText(R.string.biskajatra_title));
-//                adapter.addFragment(new FamousHereFragment("Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail."), "tab 1");
-//                adapter.addFragment(new MapFragment("hi you !!"), "tab 2");
-//                adapter.addFragment(new DetailedFragment("Hello everyone"), "tab 3");
-//                break;
+            case 16:
+                titletext.setText("Biska Jatra");
+                adapter.addFragment(new FamousHereFragment("Folks from various parts of Madhyapur Thimi gather, carrying their own chariots in Layeku Thimi.",R.drawable.biska1jatra), "Sindur Jatra");
+                adapter.addFragment(new MapFragment("An approximately 25 meter Yoh si Dyo is erected in the yosi khyo in new year eve aand is pulled down in its own way in evening of new year.",R.drawable.biska2jatra), "Lyo Si Dyo");
+                adapter.addFragment(new DetailedFragment("Is stoodup at Morning of new year eve. After it is stood up different rituals and pujas are done.It is kept standing for five days. It is stood up at pottery square.",R.drawable.biska3jatra), "Lha Maru Lyo Si Dyo");
+                break;
 //            case 17:
 //                titletext.setText(getText(R.string.pulukisi_title));
 //                adapter.addFragment(new FamousHereFragment("Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail.","Made without using single iron nail."), "tab 1");
